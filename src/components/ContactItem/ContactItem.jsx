370efbfe36f69cd36b20a8b8bb443cf4744c9835
VlikToldo <108451651+VlikToldo@users.noreply.github.com>
2023-04-07 13:55:48 +0300

    bugfix/hw

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -1,14 +1,23 @@
+import { useDispatch } from 'react-redux';
+
 import PropTypes from 'prop-types';
 
+import {removeContact} from '../../redux/contacts/contacts-slice';
 import styles from './contact-item.module.css';
 
-const ContactItem = ({ id, number, name, removeContact }) => {
+const ContactItem = ({ id, number, name}) => {
+  const dispatch = useDispatch();
+
+  const handleRemoveContact = id => {
+    dispatch(removeContact(id));
+  };
+
   return (
     <li className={styles.item}>
       {name}: {number}
       <button
         className={styles.buttonRemove}
-        onClick={() => removeContact(id)}
+        onClick={() => handleRemoveContact(id)}
         type="button"
       >
         Delete
@@ -24,7 +33,6 @@ ContactItem.defaultProps = {
 };
 
 ContactItem.propTypes = {
-  removeContact: PropTypes.func,
   id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   number: PropTypes.string.isRequired
